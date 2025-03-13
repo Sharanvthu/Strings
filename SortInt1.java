8346ec65b669c640f573efb4d1c52fa7e01f76e7
Sharanvthu <Sharanvthu@gmail.com>
2025-03-13 16:17:52 +0530

    modified on 13/03/2025

--- SortInt1.java
+++ SortInt1.java
@@ -1,26 +1,29 @@
-import java.util.Comparator;
+package segwisis;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class SortInt1 implements Comparator<T> {
-
+public class SortInt1 {
 	public static void main(String[] args) {
 
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		int[] str=new int[n];
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] str = new int[n];
 		int temp;
 		System.out.println("Original array");
-		for (int i = 0; i < str.length; i++)
-		{
-			str[i]=scan.nextInt();
+		for (int i = 0; i < str.length; i++) {
+			str[i] = scan.nextInt();
 		}
-		
-	}
+		System.out.println("Befor sort array");
+		for (int i = 0; i < str.length; i++) {
+			System.out.println(str[i]);
+		}
+		Arrays.sort(str);
+		System.out.println("Sorted array");
+		for (int i = 0; i < str.length; i++) {
+			System.out.println(str[i]);
+		}
 
-	@Override
-	public int compare(T o1, T o2) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
-
 }
+
--- StringAllMethods.java
+++ StringAllMethods.java
@@ -1,8 +1,10 @@
+package segwisis;
 
 public class StringAllMethods {
 
 	public static void main(String[] args) {
-String s="A Sharan";
-System.out.println(s.);
+		String s = "A Sharan";
+		System.out.println(s);
 
+	}
 }
